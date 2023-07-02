3b601fe07eec06c6f5556922347971b6200ecc7c
MukulDevPatel <patelmukuldev@gmail.com>
2023-07-02 18:45:24 +0530

    [Mukul Dev] Add: Using Binary search tree, we create numbers tree structure

---
+++ BinarySearchTree/CreateBinarySearchTree.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBinarySearchTree.BinarySearchTree
+{
+    public class CreateBinarySearchTree
+    {
+        public static void CreateTree()
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(56);
+            binaryTree.Add(30);
+            binaryTree.Add(70);
+            binaryTree.Add(22);
+            binaryTree.Add(40);
+            binaryTree.Add(60);
+            binaryTree.Add(95);
+            binaryTree.Add(11);
+            binaryTree.Add(65);
+            binaryTree.Add(3);
+            binaryTree.Add(16);
+            binaryTree.Add(63);
+            binaryTree.Add(67);
+
+            Console.WriteLine("Size of the binary tree: " + binaryTree.Size());
+            Console.WriteLine("Height of the binary tree: " + binaryTree.Height());
+
+            Console.WriteLine("Binary Tree:");
+            binaryTree.DisplayTree();
+        }
+    }
+    public class BinaryTree
+    {
+        private Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void Add(int data)
+        {
+            root = AddRecursive(root, data);
+        }
+
+        private Node AddRecursive(Node current, int data)
+        {
+            if (current == null)
+            {
+                return new Node(data);
+            }
+
+            if (data < current.Data)
+            {
+                current.Left = AddRecursive(current.Left, data);
+            }
+            else if (data > current.Data)
+            {
+                current.Right = AddRecursive(current.Right, data);
+            }
+
+            return current;
+        }
+
+        public int Size()
+        {
+            return CountNodes(root);
+        }
+
+        private int CountNodes(Node current)
+        {
+            if (current == null)
+            {
+                return 0;
+            }
+
+            return 1 + CountNodes(current.Left) + CountNodes(current.Right);
+        }
+
+        public int Height()
+        {
+            return CalculateHeight(root);
+        }
+
+        private int CalculateHeight(Node current)
+        {
+            if (current == null)
+            {
+                return 0;
+            }
+
+            int leftHeight = CalculateHeight(current.Left);
+            int rightHeight = CalculateHeight(current.Right);
+
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+
+        public void DisplayTree()
+        {
+            DisplayTreeRecursive(root, 0);
+        }
+
+        private void DisplayTreeRecursive(Node current, int level)
+        {
+            if (current == null)
+            {
+                return;
+            }
+
+            DisplayTreeRecursive(current.Right, level + 1);
+
+            Console.WriteLine(new string(' ', level * 4) + current.Data);
+
+            DisplayTreeRecursive(current.Left, level + 1);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using HashTableAndBinarySearchTree.HashTableProblems;
+using HashTableAndBinarySearchTree.BinarySearchTree;
 
 namespace HashTableAndBinarySearchTree
 {
@@ -12,7 +13,7 @@ namespace HashTableAndBinarySearchTree
             {
                 Console.WriteLine("\nSelect an option from below in the list");
                 Console.WriteLine("1. Find frequency of Words\n2. Find Frequency Form large Paragraph\n3. Remove Avoidable Word");
-                Console.WriteLine("BST Options\n4. Add Number using BST\n5.Exit");
+                Console.WriteLine("BST Options\n4. Add Number using BST\n5. Create a Bibnary tree\n6. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -29,6 +30,9 @@ namespace HashTableAndBinarySearchTree
                         AddNumUsingBST.AddNumber();
                         break;
                     case 5:
+                        CreateBinarySearchTree.CreateTree();
+                        break;
+                    case 6:
                         flag = false;
                         break;
                 }
